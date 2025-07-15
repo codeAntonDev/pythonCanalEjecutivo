7811b4891e70a38f899158d5bae28a6d00159d6e
Antonio Ramirez <hanscr.dsi.developer@gmail.com>
2025-07-15 14:47:02 -0500

    UPDATE: No exercises resolved - Antonio Ramirez - Python CE

--- Sesión01/Introducción a Python/r_ejercicio.py
+++
@@ -1,4 +0,0 @@
-# TODO: IMPRIME TUS NOMBRES COMPLETOS EN CONSOLA CON UN PRINT, Y UN MENSAJE EN QUE INSTITUCION ESTAS APRENDIENDO PYTHON EN UN PRINT, Utiliza secuencias de ESCAPE.
-if True:
-    print("Hola mi nombre es: \n\t\t\t\t\t/Antonio Ramirez\\")
-    print("Estoy aprendiendo Python en: \n\t\t\t\t\t\"Canal Ejecutivo\"")
